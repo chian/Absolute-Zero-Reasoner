22d8a6064bb8d96660bb40b10bde3a4284a3309d
Nicholas Chia <chuh.chia@gmail.com>
2025-06-17 21:21:15 -0500

    changes

--- verl/examples/ppo_trainer/run_gemma.sh
+++ verl/examples/ppo_trainer/run_gemma.sh
@@ -1,13 +1,13 @@
 set -x
 
 python3 -m verl.trainer.main_ppo \
-    data.train_files=$HOME/data/gsm8k/train.parquet \
-    data.val_files=$HOME/data/gsm8k/test.parquet \
+    data.train_files=$DATA/gsm8k/train.parquet \
+    data.val_files=$DATA/gsm8k/test.parquet \
     data.train_batch_size=512 \
     data.val_batch_size=1312 \
     data.max_prompt_length=1024 \
     data.max_response_length=512 \
-    actor_rollout_ref.model.path=google/gemma-2-2b-it \
+    actor_rollout_ref.model.path=$MODELS/gemma-2-2b-it \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=False \
     actor_rollout_ref.actor.ppo_mini_batch_size=128 \
